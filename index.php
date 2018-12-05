b7e6e44cbb2609dd9368ba0ad53a835aa3b24b5c
t-k- <clock126@126.com>
2018-12-04 22:27:56 -0500

    added viewport meta.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 <title>tk 的博客</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
 <meta name="author" content="Zhong Wei">
 <link href="favicon.ico" rel="SHORTCUT ICON">
 
